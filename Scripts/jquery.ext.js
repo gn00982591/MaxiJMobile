36478cfa2b2f5b60bec02dc296b22adc11b92819
gn00982591 <gn00982591@msn.com>
2015-11-18 16:44:09 +0800

    2015111801

---
+++ Scripts/jquery.ext.js
@@ -0,0 +1,6 @@
+﻿(function ($, undefined) {
+    $.fn.extend({
+        /*select設定「全部」選項*/
+        "selecteAll": function () { return this.each(function () { $(this).html("").append("<option selected='selected'>全部</option>"); }); }
+    });
+})(jQuery);
