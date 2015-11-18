af6827e8726e36cde8724bbb1bacc82de3049b14
gn00982591 <gn00982591@msn.com>
2015-11-18 18:06:44 +0800

    2015111803

--- Scripts/jquery.ext.js
+++ Scripts/jquery.ext.js
@@ -1,6 +1,10 @@
 ﻿(function ($, undefined) {
+    $.extend({
+        /*跳出訊息*/
+        "alert": function (str) { $("#dialog").text(str).dialog({ autoOpen: true, modal: true }); }
+    });
     $.fn.extend({
         /*select設定「全部」選項*/
-        "selecteAll": function () { return this.each(function () { $(this).html("").append("<option selected='selected'>全部</option>"); }); }
+        "selecteAll": function () { return this.each(function () { $(this).html("").append("<option selected='selected'>全部</option>"); }); }        
     });
 })(jQuery);
