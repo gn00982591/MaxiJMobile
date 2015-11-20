e18b07cecd7d0e84c685890cdb55a8f56bd0074c
gn00982591 <gn00982591@msn.com>
2015-11-20 11:48:53 +0800

    2015112001

--- Scripts/jquery.ext.js
+++ Scripts/jquery.ext.js
@@ -5,6 +5,25 @@
     });
     $.fn.extend({
         /*select設定「全部」選項*/
-        "selecteAll": function () { return this.each(function () { $(this).html("").append("<option selected='selected'>全部</option>"); }); }        
+        "selecteAll": function () { return this.each(function () { $(this).html("").append("<option selected='selected'>全部</option>"); }); }
     });
 })(jQuery);
+
+$(window).load(function () {
+    preloadersec = preloadersec <= 0 ? 500 : preloadersec;
+    $('.preloader__img').fadeOut(preloadersec);
+    setTimeout(function () { $('.preloader').addClass('active').delay(preloadersec * 2).fadeOut(preloadersec); }, (preloadersec * 2));
+    /*ajax 全域設定*/
+    $.ajaxSetup({
+        "beforeSend": function () {
+            var obj = $(".dvbtn");
+            obj.children().hide();
+            obj.append($("<img />", { id: "load", alt: "讀取中…", title: "讀取中…", src: "/Content/images/loading.gif" }));
+        },
+        "complete": function (d) {
+            $("#load").remove();
+            $(".dvbtn").children().show();
+        },
+        "error": function (d) { $("#debug").text(d.responseText); }
+    });
+});
